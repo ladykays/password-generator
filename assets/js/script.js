a44229bde342e006fb65d2045289c1738f558042
Kerrion Siokwu <kerrionsiokwu@Kerrions-Mini.broadband>
2022-12-28 18:25:08 +0000

    edit link to css file and js file. Add function to prompt user for password options

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,13 +12,71 @@ var upperCasedCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M',
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+  // The parseInt function is used to parse the user's input and convert it to an integer
+  var passwordLength = parseInt(prompt("How many characters would you like for your password?\nPassword must be a minimum of 10 and a maximum of 64 characters!"));
 
+  // Validate user response to 'passwordlength' prompt
+    // isNaN function is used to validate if 'passwordLength' is not a number
+    if (isNaN(passwordLength)) {
+      alert("Password length must be a NUMBER between 10 and 64!");
+    }
+    // Check if the value of  'passwordlength' variable is a number that is between 10 and 64 
+    if (passwordLength < 10 || passwordLength > 64) {
+      alert("Password must be a minimum of 10 and a maximum of 64 characters long!");
+      return;
+    }
+
+  // Create an object to store the password options indicated by the user. Properties have been set t false cos none of them has been selected by the user
+  var passwordOptions = {
+    passwordLength: passwordLength,
+    hasLowerCase: false,
+    hasUpperCase: false,
+    hasNumbers: false,
+    hasSpecials: false
+  };
+
+  // Confirm characters to include
+  passwordOptions.hasLowerCase = confirm("Would you like to include lowercase characters in your password?");
+  passwordOptions.hasUpperCase = confirm("Would you like to include uppercase characters in your password?");
+  passwordOptions.hasNumbers = confirm("Would you like to include numerical characters in your password?");
+  passwordOptions.hasSpecials = confirm("Would you like to include special characters in your password?");
+
+  // Validate user response to password options 
+   // Use a for loop to continue asking the user about the types of characters to include in the password until at least one of the properties of the object is 'true'
+   for (;;) { // The three parts of the loop are left empty  so the loop will run indefinitely until a specific condition is met
+    if (passwordOptions.hasLowerCase || passwordOptions.hasUpperCase || passwordOptions.hasNumbers || passwordOptions.hasSpecials) {
+      break;
+    }
+    alert("You must select at least one character type to include in your password!");
+    passwordOptions.hasLowerCase = confirm("Would you like to include lowercase characters in your password?");
+    passwordOptions.hasUpperCase = confirm("Would you like to include uppercase characters in your password?");
+    passwordOptions.hasNumbers = confirm("Would you like to include numerical characters in your password?");
+    passwordOptions.hasSpecials = confirm("Would you like to include special characters in your password?");
+  }
+
+    /*
+  // Use a while loop to continue asking the user about the types of characters to include in the password untill at least one of the properties of the object is 'true'
+  
+  while ((passwordOptions.hasLowerCase === false) &&(passwordOptions.hasUpperCase === false) &&(passwordOptions.hasNumbers === false) &&(passwordOptions.hasSpecials === false)) 
+  {
+    alert("You must select at leat one character type to include in your password!");
+    passwordOptions.hasLowerCase = confirm("Would you like to include lowercase characters in your password?");
+    passwordOptions.hasUpperCase = confirm("Would you like to include uppercase characters in your password?");
+    passwordOptions.hasNumbers = confirm("Would you like to include numerical characters in your password?");
+    passwordOptions.hasSpecials = confirm("Would you like to include special characters in your password?");
+  }
+  */
+
+  // Return password options
+  return passwordOptions;
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
 
-}
+} //Function getPasswordOption closes
+
+console.log(getPasswordOptions());
 
 // Function to generate password with user input
 function generatePassword() {
